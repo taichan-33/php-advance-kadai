2953d66eef3f27087cdce73ffa2954f0d01f6e1a
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2025-08-02 01:17:28 +0900

    Update update.php

--- kadai_016/php-book-app/update.php
+++ kadai_016/php-book-app/update.php
@@ -91,7 +91,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>書籍編集</title>
     <link rel="stylesheet" href="css/style.css">
     <!-- Google Fontsの読み込み -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
